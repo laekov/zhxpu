9e4e6fa4bcd3155bb3f5d2210206e6401f748fb2
zhonghaoxi <zhonghaoxi@yeah.net>
2017-11-13 20:48:08 +0800

    Init

---
+++ pc_reg.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    20:19:51 11/13/2017 
+// Design Name: 
+// Module Name:    pc_reg 
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module pc_reg(
+		input clk,
+		input rst,
+		output [15:0] 
+    );
+
+
+endmodule
