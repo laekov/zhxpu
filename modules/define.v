11cc4fb86a6665a1009dd86a168243df380d8d0f
zhonghaoxi <zhonghaoxi@yeah.net>
2017-11-21 15:00:28 +0800

    寄存器的读取访问写完了，今天完成了两个steam成就

---
+++ modules/define.v
@@ -0,0 +1,11 @@
+//Define of basics
+`define Reseting 1'b0
+`define Readable 1'b1
+`define Writeable 1'b1
+
+`define ZeroValue 15'b0
+
+//Define of registers
+`define	RegAddr	3:0
+`define RegValue 15:0
+`define ZeroReg 4'b0000
---
+++ modules/register.v
@@ -0,0 +1,90 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date:    13:01:?? 11/21/1900
+// Design Name: 
+// Module Name:    register
+// Project Name: 
+// Target Devices: 
+// Tool versions: 
+// Description: 
+//
+// Dependencies: 
+//
+// Revision: 
+// Revision 0.01 - File Created
+// Additional Comments: 
+//
+//////////////////////////////////////////////////////////////////////////////////
+module register(
+	input clk,
+	input rst,
+
+	input writable,
+	input [`RegAddr] write_addr,
+	input [`RegValue] write_value,
+
+	input readable1,
+	input [`RegAddr] read_addr1,
+	output [`RegValue] read_value1,
+
+	input readble2,
+	input [`RegAddr] read_addr2,
+	output [`RegValue] read_value2
+    );
+
+	reg [`RegValue] regs[0:15];
+
+	always @(posedge clk) begin
+		if (rst != `Reseting) begin
+			if (writable == `Writeable and write_addr != `ZeroReg) begin
+				regs[write_addr] <= write_value;
+			end
+		end
+	end
+
+	always @(*) begin
+		if (rst == `Reseting) begin
+			read_value1 <= `ZeroValue;
+		end
+		else if (readble1 == `Readable) begin
+			if (read_addr1 == `ZeroReg) begin
+				read_value1 <= `ZeroValue;
+			end
+			else if (read_addr1 == write_addr and writable == `Writeable) begin
+				read_value1 <= write_value;
+			end
+			else begin
+				read_value1 <= regs[read_addr1];
+			end
+		end
+		else  begin
+			read_value1 <= `ZeroValue;
+		end
+	end
+
+	always @(*) begin
+		if (rst == `Reseting) begin
+			read_value2 <= `ZeroValue;
+		end
+		else if (readble2 == `Readable) begin
+			if (read_addr2 == `ZeroReg) begin
+				read_value2 <= `ZeroValue;
+			end
+			else if (read_addr2 == write_addr and writable == `Writeable) begin
+				read_value2 <= write_value;
+			end
+			else begin
+				read_value2 <= regs[read_addr1];
+			end
+		end
+		else  begin
+			read_value2 <= `ZeroValue;
+		end
+	end
+
+
+endmodule
+
