2f4d6cc52c5c2d522a31896ddaa9b1360f5f0c93
zhonghaoxi <zhonghaoxi@yeah.net>
2017-11-30 18:43:43 +0800

    gg

--- kernel/c.cpp
+++ kernel/c.cpp
@@ -10,6 +10,7 @@ int main()
 {
 	FILE *infile = fopen("kernel.bin","rb");
 	freopen("kernel.out","w",stdout);
+//	FILE *outfile = fopen("kernel.chr","wb");
 	int rc;   
     unsigned char buf[MAXLEN];  
 	while( (rc = fread(buf,sizeof(unsigned char),MAXLEN,infile)) != 0 )  
@@ -17,6 +18,9 @@ int main()
 		for (int a=MAXLEN-1;a>=0;a--)
 			printf("%X%X",buf[a]/16,buf[a]%16);
 		printf("\n");
+		//for (int a=0;a<MAXLEN;a++)
+		//	printf("%c",buf[a]);
+		//	fwrite(&buf[a],sizeof(unsigned char),1,outfile);
 	}
 
 	return 0;
