b0be2b906892cee94ddb6b9f0b3dd8d4739b8086
Bakser <wangxz098@gmail.com>
2017-12-02 19:49:43 +0800

    fix???

--- modules/bootloader.v
+++ modules/bootloader.v
@@ -28,21 +28,19 @@ module bootloader(
 	reg boot_done=1'b0;
 	assign boot_done_out=boot_done;
 	reg [22:1] new_caddr;
-
+	
 	always @(*) begin
-		new_caddr <= caddr + 22'b1;
+		caddr = {4'b0, maddr + 18'b1};
 	end
-
+	
 	always @(posedge clk) begin
 		if (!rst) begin
-			caddr <= 22'b0;
 			maddr <= 18'b0;
 			boot_done <= 1'b0;
 		end else if (boot_done) begin
 			need_to_work <= 1'b0;
 		end else if (work_done == 1'b1) begin
-			maddr <= new_caddr[18:1] - 18'b1;
-			caddr <= new_caddr;
+			maddr <= caddr[18:1];
 			data <= flash_data;
 			write_ctrl <= 1'b1;
 			if(caddr > 22'h219)begin
